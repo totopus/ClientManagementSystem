4d88feade0c02503e74c690ed5fbe81f2719391c
totopus <90653223+totopus@users.noreply.github.com>
2021-11-17 12:06:50 -0500

    Add files via upload

---
+++ client.sql
@@ -0,0 +1,22 @@
+
+insert into Client values('Jack','jack@gmail.com','2121233212','93rd street','11/11/2021')
+insert into Client values('Tom','tom@gmail.com','12312312','madison ave','11/12/2021')
+insert into Client values('Aven','aven@gmail.com','444442111','park ave','11/13/2021')
+insert into Client values('Jim','jim@gmail.com','23212133212','1st ave','11/14/2021')
+insert into Client values('Tipi','tipi@gmail.com','1349810394','york ave','11/15/2021')
+insert into Client values('Mary','mary@gmail.com','1234831935','2nd ave','11/16/2021')
+
+insert into Employee values('Biag','1212122','clean')
+insert into Employee values('Topo','2323212','cook')
+insert into Employee values('Black','1212122','play')
+insert into Employee values('Gray','2323212','sing')
+insert into Employee values('Tiger','1212122','protest')
+insert into Employee values('White','2323212','sleep')
+
+insert into interaction values(5,1,'T','11/12/2021','Telephone Meeting')
+insert into interaction values(6,5,'S','11/12/2021','Skype Meeting')
+insert into interaction values(7,6,'I','11/12/2021','In Person Meeting')
+insert into interaction values(8,7,'W','11/12/2021','WebEx Meeting')
+insert into interaction values(9,8,'T','11/12/2021','Telephone Meeting')
+
+
